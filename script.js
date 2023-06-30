690a9ff96937eb152f75d313c8035c3460474dcb
Sonal2808 <135756443+Sonal2808@users.noreply.github.com>
2023-06-30 14:36:50 -0500

    Bonus mission done

--- script.js
+++ script.js
@@ -2,19 +2,26 @@ window.addEventListener("load", function() {
   this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
     return response.json();
 }).then( function(response) {
+  //sorting
+  let sortedAstronauts = response.sort(function(a,b){return b.hoursInSpace-a.hoursInSpace});
    //console.log(response); 
    //push out each astronaut to the screen  
    let myContainer = document.getElementById("container");
+   //condition if its true or false
    let allMyHtml ="";
-   for (let i=0;i<response.length;i++){
+let myCount=`
+<h3>Astronaut Count:${sortedAstronauts.length}</h3>`
+allMyHtml += myCount;
+
+     for(let i=0;i<response.length;i++){
         let myHtml=`
     <div class="astronaut">
     <div class="bio">
        <h3>${response[i].firstName}${response[i].lastName}</h3>
        <ul>
           <li>Hours in space:${response[i].hoursInSpace}</li>
-          <li>Active: ${response[i].active}</li>
-          <li>Skills: ${response[i].skills}</li>
+          <li ${response[i].active ? 'style="color:green;" ': ''}>Active: ${response[i].active}</li>
+          <li>Skills: ${response[i].skills.join(", ")}</li>
        </ul>
     </div>
     <img class="avatar" src="${response[i].picture}">
