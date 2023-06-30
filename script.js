67760f5533be904f97dc65a826d8c4656eddd74b
Sonal2808 <135756443+Sonal2808@users.noreply.github.com>
2023-06-30 10:26:13 -0500

    edited

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // TODO: add code here
+
+      window.addEvenListener("Load",function(){
+        fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+            console.log(response);
+            response.json().then(function(json){
+                console.log(json)
+            });
+        });
+
+      });
+    
